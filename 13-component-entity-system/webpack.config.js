eaa9a1654d9097b537b068124bb0ac330064ed9b
Damjan Kovacic <letecnez@gmail.com>
2019-10-02 20:20:02 +0200

    13 new project

---
+++ 13-component-entity-system/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.ts',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ['file-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: '13 Component Entity System',
+			meta: { viewport: 'width=device-width, user-scalable=no' }
+		})
+	],
+	watch: true
+};
