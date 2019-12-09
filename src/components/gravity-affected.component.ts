af9366603c9c70c6bcd9243e0858bde8ed1bcd9c
Damjan Kovacic <letecnez@gmail.com>
2019-12-09 21:44:08 +0100

    Removed legacy planet trailing

--- src/components/gravity-affected.component.ts
+++ src/components/gravity-affected.component.ts
@@ -6,18 +6,11 @@ export default class GravityAffectedComponent implements IComponent {
     public velocity: Vector2; // pixels per milisecond
     public mass: number;
     public netAcceleration: Vector2;
-    public positionHistory: Array<IPositionLog>;
 
     constructor(options: { preUpdatedPosition?: Vector2; velocity?: Vector2; mass?: number } = {}) {
         this.preUpdatedPosition = options.preUpdatedPosition || new Vector2(0, 0);
         this.velocity = options.velocity || new Vector2(0, 0);
         this.mass = options.mass || 0;
         this.netAcceleration = new Vector2(0, 0);
-        this.positionHistory = [];
     }
-}
-
-export interface IPositionLog {
-    t: number;
-    position: Vector2;
 }
--- src/systems/gravity.system.ts
+++ src/systems/gravity.system.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '../ioc/injector';
 import EntityContainer from '../entity/entity-container';
-import { OnUpdate, OnBeforeUpdate, OnRender } from '../util/lifecycle';
+import { OnUpdate, OnBeforeUpdate } from '../util/lifecycle';
 import { ILoopInfo } from '../providers/game-loop.provider';
 import TransformComponent from '../components/transform.component';
 import GravityAffectedComponent from '../components/gravity-affected.component';
 import Vector2 from '../util/vector-2';
 import CanvasProvider from '../providers/canvas.provider';
-import CameraComponent from '../components/camera.component';
 
 @Injectable()
-export default class GravitySystem implements OnBeforeUpdate, OnUpdate, OnRender {
+export default class GravitySystem implements OnBeforeUpdate, OnUpdate {
     private readonly G: number = 0.00000000075;
 
     constructor(private entityContainer: EntityContainer, private canvasProvider: CanvasProvider) {}
@@ -51,69 +50,6 @@ export default class GravitySystem implements OnBeforeUpdate, OnUpdate, OnRender
 
             const deltaPosition = gravityAffectedComponent.velocity.scale(loopInfo.dt);
             transformComponent.position = transformComponent.position.add(deltaPosition);
-
-            gravityAffectedComponent.positionHistory.push({ t: loopInfo.t, position: transformComponent.position });
-            gravityAffectedComponent.positionHistory = gravityAffectedComponent.positionHistory.filter(
-                log => log.t > loopInfo.t - 3000
-            );
-        });
-    }
-
-    public onRender(loopInfo: ILoopInfo) {
-        this.entityContainer.getEntitiesWithComponents(TransformComponent, GravityAffectedComponent).forEach(planet => {
-            const gravityAffectedComponent = planet.get(GravityAffectedComponent);
-
-            if (gravityAffectedComponent.positionHistory.length < 2) {
-                return;
-            }
-            const history = gravityAffectedComponent.positionHistory;
-            const ctx = this.canvasProvider.Context;
-
-            this.entityContainer.getEntitiesWithComponents(CameraComponent, TransformComponent).forEach(camera => {
-                const cameraTransform = camera.get(TransformComponent);
-
-                ctx.beginPath();
-                for (let i = 0; i < gravityAffectedComponent.positionHistory.length - 1; i++) {
-                    ctx.moveTo(
-                        history[i].position.x - cameraTransform.position.x - 1,
-                        history[i].position.y - cameraTransform.position.y - 1
-                    );
-                    ctx.lineTo(
-                        history[i + 1].position.x - cameraTransform.position.x - 1,
-                        history[i + 1].position.y - cameraTransform.position.y - 1
-                    );
-                }
-                ctx.strokeStyle = `#FF00FF20`;
-                ctx.stroke();
-
-                ctx.beginPath();
-                for (let i = 0; i < gravityAffectedComponent.positionHistory.length - 1; i++) {
-                    ctx.moveTo(
-                        history[i].position.x - cameraTransform.position.x + 1,
-                        history[i].position.y - cameraTransform.position.y + 1
-                    );
-                    ctx.lineTo(
-                        history[i + 1].position.x - cameraTransform.position.x + 1,
-                        history[i + 1].position.y - cameraTransform.position.y + 1
-                    );
-                }
-                ctx.strokeStyle = `#00FFFF20`;
-                ctx.stroke();
-
-                ctx.beginPath();
-                for (let i = 0; i < gravityAffectedComponent.positionHistory.length - 1; i++) {
-                    ctx.moveTo(
-                        history[i].position.x - cameraTransform.position.x,
-                        history[i].position.y - cameraTransform.position.y
-                    );
-                    ctx.lineTo(
-                        history[i + 1].position.x - cameraTransform.position.x,
-                        history[i + 1].position.y - cameraTransform.position.y
-                    );
-                }
-                ctx.strokeStyle = `#FFFFFF20`;
-                ctx.stroke();
-            });
         });
     }
 }
