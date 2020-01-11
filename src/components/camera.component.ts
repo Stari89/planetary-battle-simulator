be5ab74c41b126e17266ef1d8a55c3b773af2aaf
Damjan Kovacic <letecnez@gmail.com>
2020-01-11 12:47:25 +0100

    Refactored camera system

--- src/components/camera.component.ts
+++ src/components/camera.component.ts
@@ -4,9 +4,15 @@ import Vector2 from '../util/vector-2';
 export default class CameraComponent implements IComponent {
     public viewFramePosition: Vector2;
     public viewFrameSize: Vector2;
+    public currentZoomLevel: number;
+    public readonly maxZoomLevel: number;
+    public readonly minZoomLevel: number;
 
-    constructor() {
+    constructor(options: { minZoomLevel?: number; maxZoomLevel?: number; currentZoomLevel?: number } = {}) {
         this.viewFramePosition = new Vector2(0, 0);
         this.viewFrameSize = new Vector2(0, 0);
+        this.currentZoomLevel = options.currentZoomLevel || 0;
+        this.minZoomLevel = options.minZoomLevel || -5;
+        this.maxZoomLevel = options.maxZoomLevel || 5;
     }
 }
--- src/systems/camera.system.ts
+++ src/systems/camera.system.ts
@@ -6,6 +6,7 @@ import TransformComponent from '../components/transform.component';
 import CameraComponent from '../components/camera.component';
 import InputProvider from '../providers/input.provider';
 import CanvasProvider from '../providers/canvas.provider';
+import Vector2 from '../util/vector-2';
 
 @Injectable()
 export default class CameraSystem implements OnUpdate {
@@ -17,32 +18,69 @@ export default class CameraSystem implements OnUpdate {
 
     public onUpdate(loopInfo: ILoopInfo) {
         this.entityContainer.getEntitiesWithComponents(TransformComponent, CameraComponent).forEach(cameraEntity => {
-            const transformComponent = cameraEntity.get(TransformComponent);
-            if (this.inputProvider.MouseState.pressedButtons.some(b => b === 0)) {
-                transformComponent.position = transformComponent.position.subtract(
-                    this.inputProvider.MouseState.deltaPosition.scale(1 / transformComponent.scale.x)
-                );
-            }
+            const cameraCameraComponent = cameraEntity.get(CameraComponent);
+            const cameraTransformComponent = cameraEntity.get(TransformComponent);
 
-            if (this.inputProvider.MouseState.scrollDelta.y !== 0) {
-                const mouseCameraScaledOffset = this.inputProvider.MouseState.position
-                    .subtract(this.canvasProvider.ViewSize.scale(0.5))
-                    .scale(1 / transformComponent.scale.x);
-                transformComponent.position =
-                    this.inputProvider.MouseState.scrollDelta.y > 0
-                        ? transformComponent.position.subtract(mouseCameraScaledOffset.scale(1.25 - 1))
-                        : transformComponent.position.add(mouseCameraScaledOffset.scale(1 - 0.8));
-                transformComponent.scale = transformComponent.scale.scale(
-                    this.inputProvider.MouseState.scrollDelta.y > 0 ? 0.8 : 1.25
-                );
-            }
-
-            const cameraComponent = cameraEntity.get(CameraComponent);
-            cameraComponent.viewFramePosition = this.canvasProvider.ViewSize.scale(-0.5)
-                .scale(1 / transformComponent.scale.x)
-                .add(transformComponent.position);
-
-            cameraComponent.viewFrameSize = this.canvasProvider.ViewSize.scale(1 / transformComponent.scale.x);
+            this.updateCameraTranslation(cameraTransformComponent);
+            this.updateCameraZoom(cameraCameraComponent, cameraTransformComponent);
+            this.updateCameraViewFrame(cameraCameraComponent, cameraTransformComponent);
         });
+    }
+
+    private updateCameraTranslation(cameraTransformComponent: TransformComponent): void {
+        // return if left mouse button was not pressed
+        if (!this.inputProvider.MouseState.pressedButtons.some(b => b === 0)) {
+            return;
+        }
+        cameraTransformComponent.position = cameraTransformComponent.position.subtract(
+            this.inputProvider.MouseState.deltaPosition.scale(1 / cameraTransformComponent.scale.x)
+        );
+    }
+
+    private updateCameraZoom(
+        cameraCameraComponent: CameraComponent,
+        cameraTransformComponent: TransformComponent
+    ): void {
+        // return if scroll wheel was not moved
+        if (this.inputProvider.MouseState.scrollDelta.y === 0) {
+            return;
+        }
+
+        const offsetBeforeZooming = this.getMouseToCameraWorldOffset(cameraTransformComponent);
+        this.setCurrentZoomLevel(cameraCameraComponent, this.inputProvider.MouseState.scrollDelta);
+        const newScale = Math.pow(0.8, -cameraCameraComponent.currentZoomLevel);
+        cameraTransformComponent.scale = new Vector2(newScale, newScale);
+        const offsetAfterZooming = this.getMouseToCameraWorldOffset(cameraTransformComponent);
+
+        cameraTransformComponent.position = cameraTransformComponent.position
+            .add(offsetBeforeZooming)
+            .subtract(offsetAfterZooming);
+    }
+
+    private updateCameraViewFrame(
+        cameraCameraComponent: CameraComponent,
+        cameraTransformComponent: TransformComponent
+    ): void {
+        cameraCameraComponent.viewFramePosition = this.canvasProvider.ViewSize.scale(-0.5)
+            .scale(1 / cameraTransformComponent.scale.x)
+            .add(cameraTransformComponent.position);
+        cameraCameraComponent.viewFrameSize = this.canvasProvider.ViewSize.scale(1 / cameraTransformComponent.scale.x);
+    }
+
+    private setCurrentZoomLevel(cameraComponent: CameraComponent, scrollDelta: Vector2) {
+        cameraComponent.currentZoomLevel += scrollDelta.y < 0 ? 1 : -1;
+        if (cameraComponent.currentZoomLevel > cameraComponent.maxZoomLevel) {
+            cameraComponent.currentZoomLevel = cameraComponent.maxZoomLevel;
+        }
+        if (cameraComponent.currentZoomLevel < cameraComponent.minZoomLevel) {
+            cameraComponent.currentZoomLevel = cameraComponent.minZoomLevel;
+        }
+    }
+
+    // Returns position of the mouse relative to the camera in game world
+    private getMouseToCameraWorldOffset(cameraTransformComponent: TransformComponent): Vector2 {
+        return this.inputProvider.MouseState.position
+            .subtract(this.canvasProvider.ViewSize.scale(0.5))
+            .scale(1 / cameraTransformComponent.scale.x);
     }
 }
